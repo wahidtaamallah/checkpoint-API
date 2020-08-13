d16c327f22087870d3e02b9231761011dcb42df8
wahidtaamallah <wahidtaamallah92@hotmail.com>
2020-08-13 03:34:22 +0100

    S'habituer aux API

---
+++ myapi/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UserList from './UserList'
+import './App.css';
+
+
+function App () {
+
+return (
+<div>
+<UserList/>
+</div>
+)
+}
+
+
+export default App;
---
+++ myapi/src/UserList.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import './UserList.css'
+
+function UserList () {
+const [listOfUser, setListOfUser] = useState([]);
+
+useEffect (() => {
+    const fetchData = async () => {
+        const result = await axios.get( "https://jsonplaceholder.typicode.com/users");
+        console.log(result.data);
+ setListOfUser(result.data);
+    }
+    fetchData();
+}, []);
+  return (
+    <ul>
+      {listOfUser.map((item)=> (
+     <div className='List'>
+     <li>name: {item.name}</li>
+     <li>useName: {item.userName}</li>
+     <li>email :{item.name}</li>
+     <li>address: 
+         <ul>
+     <li>street: {item.address.street}</li>
+     <li>suite: {item.address.suite}</li>
+     <li>city: {item.address.city}</li>
+     <li>zip code: {item.address.zipcode}</li>
+     <li>geo: 
+         <ul>
+     <li>lat: {item.address.geo.lat}</li>
+     <li>lng: {item.address.geo.ing}</li>
+    </ul></li>
+      </ul></li>
+      <li>phone: {item.phone}</li>
+      <li>webSite: {item.webSite}</li>
+      <li>company: 
+          <ul>
+      <li>name: {item.company.name}</li>
+      <li>catchtPhrase: {item.company.catchtPhrase}</li>
+      <li>bs: {item.company.bs}</li>
+      </ul>
+      </li>
+      <hr/>
+      </div>
+      ))}
+          </ul>
+      )
+}
+
+export default UserList
